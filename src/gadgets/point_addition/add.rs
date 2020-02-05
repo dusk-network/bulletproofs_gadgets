b7cc7de443c87fdb050516bf332064ebabb0c07a
CPerezz <carlos@dusk.network>
2020-02-05 03:22:49 +0100

    Impl test for add_gadget

--- src/gadgets/point_addition/add.rs
+++ src/gadgets/point_addition/add.rs
@@ -1,9 +1,25 @@
 use bulletproofs::r1cs::{ConstraintSystem, LinearCombination as LC, Variable};
-use zerocaf::edwards::{AffinePoint, EdwardsPoint};
-use zerocaf::field::FieldElement as Fq;
+use curve25519_dalek::scalar::Scalar;
+use zerocaf::field::FieldElement;
 use zerocaf::ristretto::RistrettoPoint;
 
-pub fn point_addition_gadget(
+pub fn fq_as_scalar(elem: FieldElement) -> Scalar {
+    Scalar::from_bytes_mod_order(elem.to_bytes())
+}
+
+pub fn commit_point_coords(
+    cs: &mut ConstraintSystem,
+    point: RistrettoPoint,
+) -> (Variable, Variable, Variable, Variable) {
+    let p_x = cs.allocate(Some(fq_as_scalar(point.0.X))).unwrap();
+    let p_y = cs.allocate(Some(fq_as_scalar(point.0.Y))).unwrap();
+    let p_z = cs.allocate(Some(fq_as_scalar(point.0.Z))).unwrap();
+    let p_t = cs.allocate(Some(fq_as_scalar(point.0.T))).unwrap();
+
+    (p_x, p_y, p_z, p_t)
+}
+
+pub fn add_point_addition_gadget(
     cs: &mut ConstraintSystem,
     (p1_x, p1_y, p1_z, p1_t): (Variable, Variable, Variable, Variable),
     (p2_x, p2_y, p2_z, p2_t): (Variable, Variable, Variable, Variable),
@@ -45,4 +61,52 @@ pub fn point_addition_gadget(
         Variable::from(cs.multiply(F, G).2),
         Variable::from(cs.multiply(E, H).2),
     )
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use bulletproofs::r1cs::Prover;
+    use bulletproofs::{BulletproofGens, PedersenGens};
+    use merlin::Transcript;
+    use rand::thread_rng;
+    use zerocaf::edwards::{AffinePoint, EdwardsPoint};
+    use zerocaf::field::FieldElement as Fq;
+
+    #[test]
+    fn point_addition_gadget() {
+        let mut rng = thread_rng();
+
+        let gens = PedersenGens::default();
+        let mut transcript = Transcript::new(b"Testing");
+
+        let mut prover = Prover::new(&gens, &mut transcript);
+
+        let p1 = RistrettoPoint::new_random_point(&mut rng);
+        let p1_commits = commit_point_coords(&mut prover, p1);
+        let p2 = RistrettoPoint::new_random_point(&mut rng);
+        let p2_commits = commit_point_coords(&mut prover, p2);
+        let p_res = p1 + p2;
+        let a = zerocaf::constants::EDWARDS_A;
+        let a_comm = prover.allocate(Some(fq_as_scalar(a))).unwrap();
+        let d = zerocaf::constants::EDWARDS_D;
+        let d_comm = prover.allocate(Some(fq_as_scalar(d))).unwrap();
+
+        let (X, Y, Z, T) =
+            add_point_addition_gadget(&mut prover, p1_commits, p2_commits, d_comm, a_comm);
+        let (X_real, Y_real, Z_real, T_real) = commit_point_coords(&mut prover, p_res);
+        // As specified on the Ristretto protocol docs:
+        // https://ristretto.group/formulas/equality.html
+        // and we are on the twisted case, we compare
+        // `X1*Y2 == Y1*X2 | X1*X2 == Y1*Y2`.
+        let (_, _, x1_y2) = prover.multiply(LC::from(X), LC::from(Y_real));
+        let (_, _, y1_x2) = prover.multiply(LC::from(Y), LC::from(X_real));
+        let constraint = x1_y2 - y1_x2;
+        prover.constrain(LC::from(constraint));
+        let prove = prover.prove(&BulletproofGens::new(32, 1)).unwrap();
+        let verif = bulletproofs::r1cs::Verifier::new(&mut transcript);
+        assert!(verif
+            .verify(&prove, &gens, &BulletproofGens::new(32, 1), &mut rng)
+            .is_ok())
+    }
 }
